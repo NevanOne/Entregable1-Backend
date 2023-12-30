3d78860919441f99550667a68a188d00d0473226
NevanOne <gaalfonzo@gmail.com>
2023-12-30 15:11:15 -0300

    Finalizado Multer

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const ProductManager = require('./productManager');
+const ProductManager = require('./ProductManager');
 
 // Ruta al archivo de persistencia de productos
 const filePath = 'products.json';
@@ -17,8 +17,10 @@ const verificarProductos = (req, res, next) => {
   }
 };
 
-// Ruta para obtener todos los productos
-app.get('/products', verificarProductos, (req, res) => {
+// Manejo de productos
+const productsRouter = express.Router();
+
+productsRouter.get('/', verificarProductos, (req, res) => {
   const limit = parseInt(req.query.limit);
   const allProducts = products.getProducts();
 
@@ -29,17 +31,81 @@ app.get('/products', verificarProductos, (req, res) => {
   }
 });
 
-// Ruta para obtener un producto por ID
-app.get('/products/:id', verificarProductos, (req, res) => {
+productsRouter.get('/:id', verificarProductos, (req, res) => {
   const productId = parseInt(req.params.id);
-  const foundProduct = products.getProductById(productId);
-
-  if (foundProduct) {
+  try {
+    const foundProduct = products.getProductById(productId);
     res.json(foundProduct);
-  } else {
-    res.status(404).json({ error: 'El producto no existe' });
+  } catch (error) {
+    res.status(404).json({ error: error.message });
   }
 });
+
+productsRouter.post('/', (req, res) => {
+  const { title, description, code, price, stock, category, thumbnails } = req.body;
+
+  if (!title || !description || !code || !price || !stock || !category) {
+    return res.status(400).json({ error: 'Faltan campos obligatorios' });
+  }
+
+  const product = {
+    title,
+    description,
+    code,
+    price: parseFloat(price), // Aseguramos que price sea un número (en caso de tener ,50 o ,25 no se usa Int)
+    status: true, // Status es true por defecto, booleano.
+    stock: parseInt(stock), // Aseguramos que stock sea un número
+    category,
+    thumbnails: thumbnails || [] // Si no se proporciona thumbnails, se establece como un array vacío
+  };
+
+  try {
+    const result = products.addProduct(product);
+    res.json({ message: result });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+productsRouter.put('/:pid', (req, res) => {
+  const productId = parseInt(req.params.pid);
+  const updatedFields = req.body;
+  try {
+    const result = products.updateProduct(productId, updatedFields);
+    res.json({ message: result });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+productsRouter.delete('/:pid', (req, res) => {
+  const productId = parseInt(req.params.pid);
+  try {
+    const result = products.deleteProduct(productId);
+    res.json({ message: result });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Asociar las rutas de productos al endpoint '/api/products'
+app.use('/api/products', productsRouter);
+
+// Manejo de carritos
+const cartsRouter = express.Router();
+
+cartsRouter.get('/:cid', (req, res) => {
+  // Lógica para obtener los productos del carrito por su ID (cid)
+  res.json({ message: `Obtener los productos del carrito con ID ${req.params.cid}` });
+});
+
+cartsRouter.post('/:cid/product/:pid', (req, res) => {
+  // Lógica para agregar un producto al carrito por su ID (cid) y producto (pid)
+  res.json({ message: `Agregar el producto con ID ${req.params.pid} al carrito con ID ${req.params.cid}` });
+});
+
+// Asociar las rutas de carritos al endpoint '/api/carts'
+app.use('/api/carts', cartsRouter);
 
 // Iniciar el servidor en el puerto 8080
 const PORT = 8080;
